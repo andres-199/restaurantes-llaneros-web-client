560bc11ca5e1b53620e4c5b41e998b57cd7abc3c
andres199 <calopez@asertisas.com>
2021-04-24 18:03:51 -0500

    compras

--- src/app/compras/compras.component.ts
+++ src/app/compras/compras.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { environment } from 'src/environments/environment'
+import { Venta } from '../carrito/interfaces/venta.interface'
+import { Col } from '../components/dinamyc-crud/dinamyc-crud.component'
+import { LoginService } from '../login/login.service'
 
 @Component({
   selector: 'app-compras',
   templateUrl: './compras.component.html',
-  styleUrls: ['./compras.component.css']
+  styleUrls: ['./compras.component.css'],
 })
 export class ComprasComponent implements OnInit {
+  public columnsToDisplay = [
+    'fecha',
+    'Restaurant',
+    'productos',
+    'valor',
+    'metodo_pago',
+    'estado',
+  ]
 
-  constructor() { }
+  public cols: Col[] = [
+    { header: 'FECHA', field: 'fecha', type: 'date', width: '15%' },
+    { header: 'RESTAURANTE', field: 'Restaurant', type: 'payment-method' },
+    { header: 'PRODUCTOS', field: 'productos', type: 'list' },
+    { header: 'VALOR', field: 'valor', type: 'money' },
+    { header: 'METODO DE PAGO', field: 'metodo_pago', type: 'payment-method' },
+    { header: 'ESTADO', field: 'estado', type: 'chips' },
+  ]
+
+  public origin
+  public iLoaded = false
+  constructor(private loginService: LoginService) {}
 
   ngOnInit(): void {
+    const user = this.loginService.user
+    this.origin = `terceros/${user.tercero_id}/compras`
+    setTimeout(() => {
+      this.iLoaded = true
+    })
   }
 
+  onLoadCompras(compras: Venta[]) {
+    compras = compras.map((compra) => {
+      compra.Restaurant.imagen = (environment.STORAGE_URL +
+        compra.Restaurant.imagen.path) as any
+
+      compra['productos'] = []
+      for (const detalle of compra.DetalleVenta) {
+        const producto = `${detalle.cantidad} ${detalle.Producto.nombre}`
+        compra['productos'].push(producto)
+      }
+
+      compra['estado'] = compra.valida
+        ? [{ text: 'Aprovada', color: 'primary' }]
+        : compra.rechazada
+        ? [{ text: 'Rechazada', color: 'warn' }]
+        : !compra.soporte_pago && !compra.metodo_pago.contra_entrega
+        ? [{ text: 'Sin Soporte de Pago', color: 'warn' }]
+        : [{ text: 'Pendiente', color: 'accent' }]
+
+      return compra
+    })
+  }
+
+  onClickCompra(compra: Venta) {}
 }
--- src/app/compras/compras.module.ts
+++ src/app/compras/compras.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ComprasComponent } from './compras.component'
 import { RouterModule, Routes } from '@angular/router'
+import { MatCardModule } from '@angular/material/card'
+import { DinamycCrudModule } from '../components/dinamyc-crud/dinamyc-crud.module'
 
 const routes: Routes = [{ path: '', component: ComprasComponent }]
 
 @NgModule({
   declarations: [ComprasComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatCardModule,
+    DinamycCrudModule,
+  ],
 })
 export class ComprasModule {}
