0c2c1c592920315a560fe008f356d9f777ff5a47
Andres Lopez <andreslopez@foodology.com.co>
2021-09-24 23:32:11 -0500

    cambios solicitados

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,10 +16,10 @@ export class HomeComponent implements OnInit {
   restaurantes: Restaurante[]
 
   imagenes: Imagen[] = [
-    {
-      path:
-        'https://restaurantesllaneros.com:3602/original/50bd0c6d-7867-4cd5-9c3c-88ff4d901ad1.webp',
-    },
+    {path:'https://restaurantesllaneros.com:3602/original/fedfc99c-9c5a-4d8e-a5c1-8d7a7877c1f0.webp'},
+    {path:'https://restaurantesllaneros.com:3602/original/87e502b5-f87a-4c39-b9d7-5726a1dfb4d5.webp'},
+    {path:'https://restaurantesllaneros.com:3602/original/8313352c-fd8e-454f-8191-eece60a1e7c3.webp'},
+    {path:'https://restaurantesllaneros.com:3602/original/50bd0c6d-7867-4cd5-9c3c-88ff4d901ad1.webp'}
    
   ]
 
